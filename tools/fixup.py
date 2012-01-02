687e6c507cd88fee031e68f5a0b4d66e55d2583b
Kurt Raschke <kurt@kurtraschke.com>
2012-01-02 10:14:58 -0500

    Add explanatory comments in fixup.py.

--- tools/fixup.py
+++ tools/fixup.py
@@ -12,8 +12,22 @@ metadata.bind = engine
 
 trainevents = TrainEvent.__table__
 
+#Some 2 and 3 trains contain a spurious stop at the 42nd St shuttle platform,
+#which should actually be the mainline platform (and no revenue trains use
+#the connection between the mainline platform and the shuttle).
+
 session.execute(trainevents.update().where(and_(trainevents.c.stopID==902, trainevents.c.routeID.in_((2,3)))).values(stopID=127))
+
+#42nd St. Shuttle trains are not identified in the data,
+#but they reliably have a route ID of 'NA' (and are the
+#only trains to use that route ID).
+
 session.execute(trainevents.update().where(trainevents.c.routeID=='NA').values(routeID='GS'))
+
+#6 Express trains are not identified in the data.  This heuristic identifies them
+#on the assumption that they will be on the middle (express) track at the three
+#stations served by the express, but it could be made more specific
+#(by checking that the time and direction of service are appropriate for the 6X, for example).
 
 te = TrainEvent.__table__.alias('te1')
 te2 = TrainEvent.__table__.alias('te2')
